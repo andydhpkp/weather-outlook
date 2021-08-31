47bb7efa5d2477b2a77c0f132fcf11fdc37ac71b
Andrew Durham <andrewdurham1094@gmail.com>
2021-08-31 14:17:35 -0600

    changed the form to work with enter

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-//make the enter key act as the search button
-var searchContent = document.querySelector('#citySearch');
-searchContent.addEventListener('keyup', event => {
-    if(event.key !== 'Enter') return; 
-    document.querySelector('#citySearchBtn').click();
-    event.preventDefault();
-})
